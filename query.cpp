58bfedec272569980f346ac83a32c5412b4a2ca3
Matheus Menezes <matheushm1798@unifei.edu.br>
2023-11-02 14:33:19 -0300

    feat: add calculate interpolated precision

--- query.cpp
+++ query.cpp
@@ -6,9 +6,11 @@
 #include <chrono>
 #include <algorithm>
 #include <cmath>
-#define total_relevant 100;
-#define image_initial 400;
-#define image_final 499;
+#define extractor_type "zernike-result.txt" //OR: colors-result.txt
+#define image_number 488 //OR: 317, 589, 600
+#define relevant_images 100
+#define first_image 400
+#define final_image 499
 
 class Object
 {
@@ -16,9 +18,9 @@ public:
     std::string name;             // Nome da imagem
     std::vector<double> features; // Vetor de características Zernike ou Color Layout
     double distance;              // Variável para armazenar a distância
-    int numberObj;
+    int number;
 
-    Object(const std::string &n, const std::vector<double> &f) : name(n), features(f), distance(0.0), numberObj(0) {}
+    Object(const std::string &n, const std::vector<double> &f) : name(n), features(f), distance(0.0), number(0) {}
 };
 
 double euclideanDistance(const std::vector<double> &v1, const std::vector<double> &v2)
@@ -30,6 +32,67 @@ double euclideanDistance(const std::vector<double> &v1, const std::vector<double
         distance += diff * diff;
     }
     return std::sqrt(distance);
+}
+
+std::pair<std::vector<double>, std::vector<double>> calculatePrecisionAndRecall(
+    const std::vector<Object> &knn_results)
+{
+    int image_initial = first_image;
+    int image_final = final_image;
+    int total_relevant = relevant_images;
+
+    std::vector<double> precision_result;
+    std::vector<double> recall_result;
+
+    int total_retrieved = 0;
+    int relevant_retrieved = 0;
+
+    for (const Object &obj : knn_results)
+    {
+        total_retrieved++;
+
+        if (obj.number >= image_initial && obj.number <= image_final)
+        {
+            relevant_retrieved++;
+        }
+
+        double precision = static_cast<double>(relevant_retrieved) / total_retrieved;
+        double recall = static_cast<double>(relevant_retrieved) / total_relevant;
+
+        precision_result.push_back(precision);
+        recall_result.push_back(recall);
+    }
+
+    return std::make_pair(precision_result, recall_result);
+}
+
+std::vector<double> calculateInterpolatedPrecision(
+    const std::vector<double> &precision_result,
+    const std::vector<double> &recall_result)
+{
+    std::vector<double> recall_axis = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+    std::vector<double> interpolated_precision;
+
+    for (const double recall_point : recall_axis)
+    {
+        double max_precision = 0.0;
+
+        for (int i = 0; i < precision_result.size(); i++)
+        {
+            if (precision_result[i] > max_precision && recall_result[i] >= recall_point)
+            {
+                max_precision = precision_result[i];
+            }
+        }
+        interpolated_precision.push_back(max_precision);
+    }
+
+    for (const double precision : interpolated_precision)
+    {
+        std::cout << precision << " ";
+    }
+
+    return interpolated_precision;
 }
 
 void rangeQuery(std::vector<Object> dataset, std::vector<double> &queryImageFeatures, double range_threshold)
@@ -80,19 +143,19 @@ void rangeQuery(std::vector<Object> dataset, std::vector<double> &queryImageFeat
     }
     // std::cout << "\nNumber images: " << numberObj << std::endl;
 
-    std::cout << "Tempo: " << duration.count() << "ms" << std::endl;
+    // std::cout << "Tempo: " << duration.count() << "ms" << std::endl;
 }
 
 void knnQuery(std::vector<Object> dataset, std::vector<double> &queryImageFeatures, int k)
 {
     std::vector<Object> knn_results;
 
-    auto start = std::chrono::high_resolution_clock::now(); // Inicia a contagem do tempo
+    //auto start = std::chrono::high_resolution_clock::now(); // Inicia a contagem do tempo
     int numberObject = 0;
     for (Object &obj : dataset)
     {
         obj.distance = euclideanDistance(obj.features, queryImageFeatures); // Atualiza a distância no objeto
-        obj.numberObj = numberObject;
+        obj.number = numberObject;
         numberObject++;
 
         if (knn_results.size() < k)
@@ -113,93 +176,23 @@ void knnQuery(std::vector<Object> dataset, std::vector<double> &queryImageFeatur
         }
     }
 
-    auto stop = std::chrono::high_resolution_clock::now();                                                            // Marca o fim da execução
-    std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::duration<double>>(stop - start); // Calcula o tempo de execução em milissegundos
+    //auto stop = std::chrono::high_resolution_clock::now();                                                            // Marca o fim da execução
+    //std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::duration<double>>(stop - start); // Calcula o tempo de execução em milissegundos
 
-    // Exibe os resultados do k-NN
-
-    std::vector<double> precision_result;
-    std::vector<double> recall_result;
-
-    int relevant_retrieved = 0;
-    int total_retrieved = 0;
-
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-
-    for (const Object &obj : knn_results) {
-        // std::cout << obj.name << " " << obj.distance << std::endl;
-        total_retrieved++;
-
-        int img_initial = image_initial;
-        int img_final = image_final;
-        if (obj.numberObj >= img_initial && obj.numberObj <= img_final)
-        {
-            relevant_retrieved++;
-        }
-
-        double precision = static_cast<double>(relevant_retrieved) / total_retrieved;
-        double recall = static_cast<double>(relevant_retrieved) / total_relevant;
-
-        precision_result.push_back(precision);
-        recall_result.push_back(recall);
-    }
-
+    // ******* Exibe os resultados do k-NN *******
+    // std::cout << "" << std::endl;
+    // std::cout << "" << std::endl;
     // for (const double precision : precision_result) {
     //     std::cout << precision << " ";
     // }
-
     // std::cout << std::endl;
-
     // for (const double recall : recall_result) {
     //     std::cout << recall << " ";
     // }
 
-
-
-
-    // for (const double recall : recall_final){
-    //     double max_precision = 0.0;
-    //     double respective_recall = 0.0;
-    //     for (const double precision_at : precision_result){
-    //         for (const double recall_at : recall_result){
-    //             if(precision_at > max_precision && recall_at >= recall){
-    //                 max_precision = precision;
-    //                 respective_recall = recall_at;
-    //             }
-    //         }
-    //     }
-
-    // }
-
-
-    std::vector<double> recall_final = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-    std::vector<double> interpolated_precision;
-    
-    double max_precision = 0.0;
-    double respective_recall = 0.0;
-
-    for (const double recall : recall_final){
-  std::cout << recall << " ";   
-
-        for (int i = 0; i < precision_result.size(); i++){
-              std::cout << recall << " ";  
-           //         std::cout << recall << " ";
-            //std::cout << precision_result[i] << " ";
-            //std::cout << recall_result[i] << " ";
-            if(precision_result[i] > max_precision && recall >= recall_result[i]){
-                max_precision = precision_result[i];
-            }
-            
-        }
-
-        interpolated_precision.push_back(max_precision);
-        //std::cout << "max: "<< max_precision << " ";
-        max_precision = 0.0;
-         
-    }
-
-
+    std::cout << "" << std::endl;
+    std::pair<std::vector<double>, std::vector<double>> result = calculatePrecisionAndRecall(knn_results);
+    std::vector<double> interpolated_precision = calculateInterpolatedPrecision(result.first, result.second);
 
     // std::cout << "Tempo: " << duration.count() << "ms" << std::endl;
 }
@@ -207,7 +200,7 @@ void knnQuery(std::vector<Object> dataset, std::vector<double> &queryImageFeatur
 int main()
 {
     // Nome do arquivo de características
-    std::string filename = "zernike-result.txt";
+    std::string filename = extractor_type;
 
     std::ifstream file(filename);
     if (!file.is_open())
@@ -230,22 +223,20 @@ int main()
 
         std::vector<double> features;
         while (iss >> featureValue)
-        { // Leitura dos valores das características (demais posições)
+        {   // Leitura dos valores das características (demais posições)
             features.push_back(featureValue);
         }
 
         dataset.push_back(Object(imageName, features));
     }
 
-    int queryImageNumber = 488;
+    int queryImageNumber = image_number;
 
     std::vector<double> &queryImageFeatures = dataset[queryImageNumber].features; // Features da imagem de consulta
 
-    int relevants = total_relevant;
-    knnQuery(dataset, queryImageFeatures, relevants);
-    // for(int i = 400; i < 500; i++){
-    //     knnQuery(dataset, dataset[i].features, 100);
-    // }
+    int image_initial = first_image;
+    int image_final = final_image;
+    int total_relevant = relevant_images;
 
     // rangeQuery(dataset, queryImageFeatures, 0.452020);
     // rangeQuery(dataset, queryImageFeatures, 0.509368);
@@ -253,6 +244,13 @@ int main()
 
     // knnQuery(dataset, queryImageFeatures, 16);
     // knnQuery(dataset, queryImageFeatures, 21);
+
+    // knnQuery(dataset, queryImageFeatures, total_relevant);
+
+    for (int i = image_initial; i <= image_final; i++)
+    {
+        knnQuery(dataset, dataset[i].features, total_relevant);
+    }
 
     return 0;
 }
